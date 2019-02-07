09be76eb0bab4b084378b5f0b479a68111409acc
Nyra Dev <37726406+akinwol@users.noreply.github.com>
2019-02-06 21:34:12 -0500

    v0.0001

---
+++ src/components/pages/About.js
@@ -0,0 +1,13 @@
+//rcf 
+import React from 'react'
+
+export default function About() {
+  return (
+      //if you don't actually need a div,  you can just use
+      //react fragment
+   <React.Fragment>
+       <h1>About</h1>
+       <p>This is the todo list app v1.0.0</p>
+   </React.Fragment>
+  )
+}
